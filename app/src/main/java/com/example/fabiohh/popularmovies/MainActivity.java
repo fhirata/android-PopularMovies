6e6665b31ae93ee593e97da02dd05eb8c9b0cae6
Fabio Hirata <fabiohh@yahoo-inc.com>
2016-08-28 14:21:12 -0700

    Added menu for toggling between Most Popular and Top Rated movie backends.

--- app/src/main/java/com/example/fabiohh/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/fabiohh/popularmovies/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fabiohh.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,4 +23,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.movies, menu);
+
+        return true;
+    }
 }
--- app/src/main/java/com/example/fabiohh/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/example/fabiohh/popularmovies/MoviesFragment.java
@@ -1,12 +1,15 @@
 package com.example.fabiohh.popularmovies;
 
 import android.app.Fragment;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
@@ -29,14 +32,21 @@ import java.util.ArrayList;
 /**
  * Created by fabiohh on 8/23/16.
  */
-public class MoviesFragment extends Fragment {
+public class MoviesFragment extends Fragment implements Preference.OnPreferenceChangeListener {
 
-    static final String MOVIE_DISCOVER_API_URL    = "http://api.themoviedb.org/3/discover/movie";
-    static final String MOVIE_TOP_RATED_API_URL   = "http://api.themoviedb.org/3/movie/top_rated";
+    static final String MOVIE_DISCOVER_API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static final String MOVIE_TOP_RATED_API_URL = "http://api.themoviedb.org/3/movie/top_rated";
     static final String MOVIE_IMAGE_URL = "http://image.tmdb.org/t/p/w185";
+    ImageAdapter mImageAdapter;
+    LayoutInflater mInflater;
+    int mApiMode;
 
     static final int MOVIE_ITEM_POSITION_CODE = 6767;
-    ImageAdapter mImageAdapter;
+
+    // Preferences
+    static final int MOVIE_API_MODE_TOPRATED = 0;
+    static final int MOVIE_API_MODE_POPULAR = 1;
+    static final String MOVIE_API_PREFERENCE = "api_mode";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -47,18 +57,46 @@ public class MoviesFragment extends Fragment {
         FetchMovieTask moviesTitleTask = new FetchMovieTask();
         moviesTitleTask.execute();
 
+        mInflater = inflater;
         mImageAdapter = new ImageAdapter(getActivity(), new ArrayList<MovieItem>());
         gridview.setAdapter(mImageAdapter);
+
+        // Use last user's preference if available
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        if (preferences.contains(MOVIE_API_PREFERENCE)) {
+            mApiMode = preferences.getInt(MOVIE_API_PREFERENCE, MOVIE_API_MODE_TOPRATED);
+        }
 
         return rootView;
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == MOVIE_ITEM_POSITION_CODE) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setHasOptionsMenu(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        switch (id) {
+            case R.id.menu_popular:
+                mApiMode = MOVIE_API_MODE_POPULAR;
+                break;
+            case R.id.menu_toprated:
+                mApiMode = MOVIE_API_MODE_TOPRATED;
+                break;
         }
+        FetchMovieTask moviesTitleTask = new FetchMovieTask();
+        moviesTitleTask.execute();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        preferences.edit().putInt(MOVIE_API_PREFERENCE, mApiMode).apply();
+
+        return super.onOptionsItemSelected(menuItem);
     }
 
     private class FetchMovieTask extends AsyncTask<String, Void, ArrayList<MovieItem>> implements IMovieInfo {
@@ -88,8 +126,8 @@ public class MoviesFragment extends Fragment {
                 throw new RuntimeException("Missing API Key. See README file.");
             }
 
-
-            String movie_api_url = MOVIE_DISCOVER_API_URL;
+            String movie_api_url = (mApiMode == MOVIE_API_MODE_TOPRATED) ?
+                    MOVIE_TOP_RATED_API_URL : MOVIE_DISCOVER_API_URL;
 
             Uri uri = Uri.parse(movie_api_url).buildUpon()
                     .appendQueryParameter(KEY_PARAM, key).build();
@@ -158,14 +196,14 @@ public class MoviesFragment extends Fragment {
 
             ArrayList<MovieItem> movieItemInfoResult = new ArrayList<>();
 
-            for(int i = 0; i < moviesArray.length(); i++) {
+            for (int i = 0; i < moviesArray.length(); i++) {
                 JSONObject movieObject = moviesArray.getJSONObject(i);
 
-                String title       = movieObject.getString(MOVIE_TITLE);
-                String imgUrl      = buildImageURL(movieObject.getString(MOVIE_IMAGE));
+                String title = movieObject.getString(MOVIE_TITLE);
+                String imgUrl = buildImageURL(movieObject.getString(MOVIE_IMAGE));
                 String description = movieObject.getString(MOVIE_DESC);
                 String voteAverage = movieObject.getString(MOVIE_VOTE_AVG);
-                String voteCount   = movieObject.getString(MOVIE_VOTE_COUNT);
+                String voteCount = movieObject.getString(MOVIE_VOTE_COUNT);
                 String releaseYear = movieObject.getString(MOVIE_YEAR);
 
                 movieItemInfoResult.add(new MovieItem(title, description, voteAverage, voteCount, imgUrl, releaseYear));
@@ -179,5 +217,12 @@ public class MoviesFragment extends Fragment {
     }
 
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        preference.setSummary(stringValue);
+        return true;
+    }
 }
 
